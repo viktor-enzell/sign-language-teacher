e3bbc3ff668bdbef76d2370157e8325d22576f47
adam <adam.v.hoewijk@gmail.com>
2021-09-30 13:49:14 +0200

    Added comments

--- preprocessing.py
+++ preprocessing.py
@@ -1,6 +1,7 @@
 
 import numpy as np
 
+# function that converts the handladmarks to a list of coordinates
 def flatten_landmarks(hand_landmarks):
     keypoints = []
     for data_point in hand_landmarks.landmark:
@@ -8,10 +9,12 @@ def flatten_landmarks(hand_landmarks):
         keypoints.append(data_point.y)
         keypoints.append(data_point.z)
     return keypoints
-    
+
+# Make sure that the array is on a scale of [0,1]
 def transform_array(array):
     return list(np.array(array) * (1 / max(array)))
 
+# Executes both the previous functions
 def preprocess_keypoints(land_marks):
     res = flatten_landmarks(land_marks)
     res = transform_array(res)
