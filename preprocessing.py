0f5c48cdb448681b9dbc1a06d2b43a2a662ac98b
adam <adam.v.hoewijk@gmail.com>
2021-11-04 13:36:46 +0100

    upc

--- preprocessing.py
+++ preprocessing.py
@@ -4,18 +4,21 @@ import numpy as np
 # function that converts the handladmarks to a list of coordinates
 def flatten_landmarks(hand_landmarks):
     keypoints = []
+    distances = []
     for data_point in hand_landmarks.landmark:
         keypoints.append(data_point.x)
         keypoints.append(data_point.y)
         keypoints.append(data_point.z)
-    return keypoints
+        distances.append(np.linalg.norm(data_point.x, data_point.y, data_point.z, ord=None, axis=None, keepdims=False))
+    return keypoints, distances
 
 # Make sure that the array is on a scale of [0,1]
-def transform_array(array):
+def normalize_array(array):
     return list(np.array(array) * (1 / max(array)))
 
 # Executes both the previous functions
 def preprocess_keypoints(land_marks):
-    res = flatten_landmarks(land_marks)
-    res = transform_array(res)
-    return res
+    keypoints, distances= flatten_landmarks(land_marks)
+    keypoints = normalize_array(keypoints)
+    distances = normalize_array(distances)
+    return keypoints + distances
--- voice_assistant.py
+++ voice_assistant.py
@@ -1,15 +1,21 @@
 import random
 import time
+
+from cv2 import QRCodeDetector
 from labels import labels
 import gtts
 from playsound import playsound
 import audioread
+import json
+import numpy as np
 
 
 class VoiceAssistant:
     current_letter = None
     finished_speaking_time = None
     has_suggested = False
+    username = None
+    c = 1.4
 
     def __init__(self):
         self.finished_speaking_time = time.time()
@@ -27,15 +33,23 @@ class VoiceAssistant:
         return self.finished_speaking_time < time.time()
 
     def welcome(self, username):
+        self.username = username
         self.text_to_speech('Welcome ' + username + '! I am your sign-language teacher. '
                             'I will tell you a letter and you can show me the corresponding sign. '
                             'Let\'s go!')
+
+    def get_upc(self):
+        upc_val = []
+        avg_q = sum(Q) / len(Q)
+        avg_q + c * np.sqrt(np.log(t/N))
+
 
     def suggest_letter(self):
         self.current_letter = random.choice(labels)
         self.text_to_speech(f'Do the sign-language sign for letter {self.current_letter.upper()}')
         self.has_suggested = True
         return self.current_letter
+
 
     def correct(self):
         self.text_to_speech('Correct!')
